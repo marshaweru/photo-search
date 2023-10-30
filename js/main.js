ef3ba1af496495e9795bd9cf09968ddfdc8f3808
weru <147871343+marshaweru@users.noreply.github.com>
2023-10-29 23:32:31 -0700

    New data lines for functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 //Add JS that will fetch images from API AND display images on website
 //Add variable for elements ids search form, search box, search result and show more button
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");

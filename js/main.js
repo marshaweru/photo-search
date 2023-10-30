8afbff3e0964304532a04b3e76e1e4d67206d16d
weru <147871343+marshaweru@users.noreply.github.com>
2023-10-30 03:46:11 -0700

    Correct from quotes to backtick

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ async function searchImages(){
     //Keyword will store value entered into input field//
     keyword = searchBox.value;
     //Add const and API urls. In API url we add page number and query text which is the keyword. We also add client ID
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
     //To get response in browser//
     const response = await fetch(url);

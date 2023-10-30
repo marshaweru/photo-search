df3452659d8c15e15713e0995fb77bf6e6b75963
weru <147871343+marshaweru@users.noreply.github.com>
2023-10-29 23:21:50 -0700

    Additional comments for functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
 //Add JS that will fetch images from API AND display images on website
-//Add variable for 
+//Add variable for elements ids search form, search box, search result and show more button

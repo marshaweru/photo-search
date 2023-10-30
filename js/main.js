f38c916963e186ebc201f0c69bcdacf73f56c029
weru <147871343+marshaweru@users.noreply.github.com>
2023-10-30 00:44:58 -0700

    Comments for adding const and API urls

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 //Add JS that will fetch images from API AND display images on website
 //Add variable for elements ids search form, search box, search result and show more button
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
 
 //Add API from where to get images by inserting API url in JS code file//
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    //Keyword will store value entered into input field//
+    keyword = searchBox.value;
+    //Add const and API urls. In API url we add page number and query text which is the keyword. We also add client ID
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+}
